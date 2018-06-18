74941b9514ff6d7869f19556ba4690fb4f194011
hiepvo-groover <hiep.vo@groovetechnology.com>
2018-06-18 18:21:47 +0700

    updated

--- WebApplication2/Controllers/DefaultController.cs
+++ WebApplication2/Controllers/DefaultController.cs
@@ -22,15 +22,21 @@ namespace WebApplication2.Controllers
                 try
                 {
                     conn.Open();
-
+                    result = new List<string>();
                     // Insert some data
-                    //using (var cmd = new NpgsqlCommand())
-                    //{
-                    //    cmd.Connection = conn;
-                    //    cmd.CommandText = "INSERT INTO data (some_field) VALUES (@p)";
-                    //    cmd.Parameters.AddWithValue("p", "Hello world");
-                    //    cmd.ExecuteNonQuery();
-                    //}
+                    using (var cmd = new NpgsqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        //default only use by not include the column in the insert statement. different from specify that column and insert NULL value into it
+                        cmd.CommandText = @"INSERT INTO dbo.eventstore (id, aggid, isdeleted, jsondata, objtype, type) VALUES (@p1 , @p2 , @p3 , @p4 , @p5,@p6)";
+                        cmd.Parameters.AddWithValue("p1",Guid.NewGuid());
+                        cmd.Parameters.AddWithValue("p2", Guid.NewGuid());
+                        cmd.Parameters.AddWithValue("p3", true);//bit not boolean
+                        cmd.Parameters.AddWithValue("p4", "");
+                        cmd.Parameters.AddWithValue("p5", "objtype");
+                        cmd.Parameters.AddWithValue("p6", 0);
+                        cmd.ExecuteNonQuery();
+                    }
 
                     // Retrieve all rows
                     using (var cmd = new NpgsqlCommand("select * from dbo.eventstore", conn))
@@ -46,7 +52,7 @@ namespace WebApplication2.Controllers
             }
             return Ok(new {
                 isReceived = true,
-                result = string.Join(',',result.ToArray())
+                result = string.Join(',',result?.ToArray())
             });
         }
 
